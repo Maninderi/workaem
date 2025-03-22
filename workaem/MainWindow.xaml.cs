05b544d9938c7c545e6f640e407fcb6e7685f7bc
Maninderi <rybalkaj@gmail.com>
2025-03-22 12:36:35 +0500

    добавил интерфейс и логику игры

--- workaem/MainWindow.xaml.cs
+++ workaem/MainWindow.xaml.cs
@@ -15,17 +15,74 @@ using System.Windows.Shapes;
 
 namespace workaem
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public partial class MainWindow : Window
         {
-            InitializeComponent();
-        }
+            private string currentPlayer = "X";
+            private string[,] board = new string[3, 3];
 
-        private void Exit_Click(object sender, RoutedEventArgs e)
+            public MainWindow()
+            {
+                InitializeComponent();
+            }
+
+            private void Button_Click(object sender, RoutedEventArgs e)
+            {
+                Button button = sender as Button;
+
+                if (button.Content == null)
+                {
+                    button.Content = currentPlayer;
+                    int row = Grid.GetRow(button);
+                    int col = Grid.GetColumn(button);
+                    board[row, col] = currentPlayer;
+
+                    if (CheckForWinner())
+                    {
+                        MessageBox.Show($"Игрок {currentPlayer} выиграл!");
+                        ResetGame();
+                    }
+                    else
+                    {
+                        currentPlayer = currentPlayer == "X" ? "O" : "X";
+                    }
+                }
+            }
+
+            private bool CheckForWinner()
+            {
+                // Проверка строк, столбцов и диагоналей
+                for (int i = 0; i < 3; i++)
+                {
+                    if (board[i, 0] == currentPlayer && board[i, 1] == currentPlayer && board[i, 2] == currentPlayer)
+                        return true;
+                    if (board[0, i] == currentPlayer && board[1, i] == currentPlayer && board[2, i] == currentPlayer)
+                        return true;
+                }
+                if (board[0, 0] == currentPlayer && board[1, 1] == currentPlayer && board[2, 2] == currentPlayer)
+                    return true;
+                if (board[0, 2] == currentPlayer && board[1, 1] == currentPlayer && board[2, 0] == currentPlayer)
+                    return true;
+
+                return false;
+            }
+
+            private void ResetGame()
+            {
+                currentPlayer = "X";
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        board[i, j] = null;
+                        Button button = (Button)FindName($"Button{i}{j}");
+                        button.Content = null;
+                    }
+                }
+            }
+
+            private void Exit_Click(object sender, RoutedEventArgs e)
         {
             Close();
         }
@@ -33,5 +90,7 @@ namespace workaem
         {
             MessageBox.Show("Разработчик: Рыбалка Евгений и Пантелеев Александр\nГруппа: Исппк-22-1", "Информация о разработчике", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
+
+
     }
 }
